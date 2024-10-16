4373d1c3f081a20e17f019842bf0fc579af27665
ingta <ing.talero@gmail.com>
2024-10-16 14:06:51 -0500

    Nuevo

---
+++ PracticaRest/PracticaRest/src/test/java/com/Rest/PracticaRest/PracticaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rest.PracticaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
