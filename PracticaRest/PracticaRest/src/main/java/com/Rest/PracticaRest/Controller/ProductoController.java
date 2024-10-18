2b4cb25662232654f054b4d5e8ca2c990f993c73
ingta <ing.talero@gmail.com>
2024-10-18 15:35:35 -0500

    Nueva Actualización

--- PracticaRest/PracticaRest/src/main/java/com/Rest/PracticaRest/Controller/ProductoController.java
+++ PracticaRest/PracticaRest/src/main/java/com/Rest/PracticaRest/Controller/ProductoController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 import java.util.UUID;
+
+/**
+ * @Autor Cristian Talero
+ */
 @Controller
 @RestController
 @RequestMapping("/api/Producto")
